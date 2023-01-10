4d59f14c81dd356ed52b133a345cc1f93403f5b4
Stacie Rogers <stacierogers92@icloud.com>
2023-01-09 23:34:17 -0600

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] 
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat() {
+    let copyOfCats = [...cats, "Broom"]
+    return copyOfCats
+}
+function prependCat() {
+    let copyOfCats = ["Arnold", ...cats]
+    return copyOfCats
+}
+function removeLastCat() {
+    let copyOfCats = cats.slice(0, cats.length - 1)
+    return copyOfCats
+}
+function removeFirstCat() {
+    let copyOfCats = cats.slice(1)
+    return copyOfCats
+}
